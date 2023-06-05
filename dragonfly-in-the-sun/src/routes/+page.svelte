d9d7c18f951539a7833e930005da2f00e8a46770
Evelin Zirakashvili <eve.zirakashvili@gmail.com>
2023-06-05 21:24:21 +0400

    create page skeleton

--- dragonfly-in-the-sun/src/routes/+page.svelte
+++ dragonfly-in-the-sun/src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to Dragonfly in the sun</h1>
-<h1 class="text-3xl font-bold underline mt-5 text-error">Hello world!</h1>
+<div class="w-screen h-full bg-main-pattern relative">
+  <div class="max-w-app-container w-full mx-auto mt-[161px]">
+    <div class="flex mb-[132px]">
+      <div class="basis-1/2">
+        <h1 class="uppercase">dragonfly in the sun</h1>
+        <p class="">
+          Boutique software and nodes for web3 and p2p technology Specialist development and
+          application of open source software to hard problems of transparency and trust in the
+          digital space.
+        </p>
+      </div>
+      <div class="basis-1/2">
+        <img width="333" src="/favicon.svg" alt="" class="" />
+      </div>
+    </div>
+    <div class="bg-white w-full px-[162px] pt-[137px] pb-[387px] relative z-30">
+      Semi Fungible Tokens (SFT) & IPFS hosting  Dragonfly has spent 2+ years developing the smart
+      contracts, indexing, automated decentralised storage node hardware and software for SFTs. 
+      Dragonfly is a primary infrastructure and development provider for Gild Lab, who use the SFT
+      software to develop industry relationships and apply the infrastructure provided by Dragonfly
+      to specific use cases.  SFTs offer a higher degree of transparency and regulatory compliance
+      for token issuers who need to represent offchain assets on the blockchain. The core innovation
+      of SFT is a hybrid ERC20/ERC1155 token system where minting and burning of an ERC20 token is
+      always pairwise with an NFT that can track evidence of the provenance of the offchain assets. 
+      Auditors can review the state of the offchain and onchain assets at any moment and
+      freeze/unfreeze the token if they are dissatisfied with the integrity of the token issuer. 
+      The hashes of all evidence used in the audit process are broadcast on the blockchain, indexed
+      by a dedicated Subgraph, then made available on the IPFS file storage network. This way,
+      anyone who is a token holder can download and retain a permanent copy of all audit information
+      for their token simply by running an IPFS node. If there is ever suspected fraud at the token
+      layer, the token holders can use the same evidence that informed the auditors historically to
+      bring a legal case in whatever jurisdiction will host it.  The system is fully open source and
+      decentralised, anyone can create an SFT token, and it is on them to adhere to all transparency
+      and regulatory requirements, as any token issuer must. Dragonfly DOES NOT issue any tokens
+      itself. It makes money in the same way as many companies that develop open source, by
+      maintaining systems and business relationships on top of the open source code. Dragonfly
+      operates physical IPFS nodes for audit trail hosting on behalf of anyone who does not want to
+      maintain the infrastructure themselves. Dragonfly also develops tools for auditors and token
+      holders to review SFT tokens permisionlessly.    Programmable SFT token liquidity &
+      governance  Dragonfly works closely with Rain protocol to develop Rainlang. Rainlang is a
+      novel language that runs on the Ethereum Virtual Machine (EVM) to allow smart contract logic
+      to be written without compiling to native EVM bytecode.  This has several transparency,
+      security and efficiency benefits, and is simply easier to write, with a learning curve much
+      closer to making an Excel spreadsheet than doing rocket science (which is what blockchain work
+      often feels like). This enables next generation decentralised application logic such as
+      programmable token governance, onchain trading strategies, escrow/insurance style products,
+      hybrid offchain/onchain business rules, etc. Rain allows SFT token issuers to more easily
+      program onchain how third parties interact with their token, a goal that is usually
+      prohibitively expensive and risky, or simply unattainable for the average token issuer.   
+      Gilded tokens  Dragonfly designed and developed Gilded Tokens with Gild Labs. Trustless
+      collateral such as ETH can be deposited, minting a second token according to a price oracle
+      such as the current gold price. The second token can be transferred and traded independent of
+      the backing collateral, with a floating price somewhere between 0 and the current mint price,
+      creating a semi-stable, but fully collateralised and non-liquidating token. Gilded Tokens are
+      a long term economic experiment, to be paired with more traditional token peg maintenance and
+      issuance as per SFT and Rain based liquidity provisioning.    Web3 expertise  Dragonfly
+      software developers have years of “full stack” expertise in open source web3 and p2p software
+      development. Notably, working with Holo and AllianceBlock for years on decentralised
+      technologies, including designing and implementing novel solutions to hard problems from the
+      ground up.
+    </div>
+  </div>
+  <img
+    height="323"
+    src="/layer-2-bkground-mountain.svg"
+    alt=""
+    class="absolute bottom-0 z-10 -right-2"
+  />
+</div>
+<img src="/layer-4-stream.svg" alt="" class="-mt-[277px] relative z-40 w-screen" />
--- dragonfly-in-the-sun/tailwind.config.js
+++ dragonfly-in-the-sun/tailwind.config.js
@@ -3,9 +3,18 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     colors: {
-      error: '#e11d48'
+      error: '#e11d48',
+      white: '#fff'
     },
-    extend: {}
+    extend: {
+      maxWidth: {
+        'app-container': '1075px'
+      },
+      backgroundImage: {
+        'main-pattern':
+          'linear-gradient(180deg, #0F4260 0%, #3E778D 7.05%, #96D3E0 15.93%, #D0DED7 26.81%, #BEC6B5 62.53%, #FFE590 100%)'
+      }
+    }
   },
   plugins: []
 }
