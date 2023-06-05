a323f08aeb2ae1a47cb5fe74fc0364d7e8a009e1
Evelin Zirakashvili <eve.zirakashvili@gmail.com>
2023-06-05 19:10:47 +0400

    add tailwind

--- dragonfly-in-the-sun/src/routes/+page.svelte
+++ dragonfly-in-the-sun/src/routes/+page.svelte
@@ -1,1 +1,2 @@
 <h1>Welcome to Dragonfly in the sun</h1>
+<h1 class="text-3xl font-bold underline mt-5 text-error">Hello world!</h1>
---
+++ dragonfly-in-the-sun/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      error: '#e11d48'
+    },
+    extend: {}
+  },
+  plugins: []
+}
